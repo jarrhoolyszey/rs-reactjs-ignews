313cbc01650f341af2240001f9a4c612378cc8f7
Emerson <jarrhoolyszey@gmail.com>
2022-06-14 18:40:15 -0300

    Verificar se Login com Github funciona fora do Proxy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { stripe } from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
+
+// Formas de acessas API do Next.js
+// Client-side Rendering (CSR)
+// Server-side Rendering (SSR)
+// Static Site Generation (SSG)
+
 
 interface HomeProps {
   product: {
@@ -36,7 +42,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1LAIdqJFcFlKGdUXuaBwBkjn');
 
   const product = {
@@ -50,6 +56,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
