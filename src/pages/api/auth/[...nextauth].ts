a3737cc9bb3d272d1fc2af5ac2c6fdbb28b6b952
Emerson <jarrhoolyszey@gmail.com>
2022-06-15 16:49:01 -0300

    Final Chapter 3 - Parte 2

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -16,4 +20,37 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-})
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: user.email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+
+          )
+        );
+
+        return true
+      } catch {
+        return true
+      }
+    },
+  },
+});
